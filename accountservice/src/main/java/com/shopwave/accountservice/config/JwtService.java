f52d78ae5b0e72b8f82c6d76b7a4dab400cf35f0
Andres Felipe Pardo Campos <andresfp292@gmail.com>
2023-09-11 20:46:38 -0500

    Implementing account service

---
+++ accountservice/src/main/java/com/shopwave/accountservice/config/JwtService.java
@@ -0,0 +1,39 @@
+package com.shopwave.accountservice.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "djwAoZrlxjH9BZGxLsT60qGPmVjyZI12";
+
+    public String extractUserEmail(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims,T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ accountservice/src/main/java/com/shopwave/accountservice/repository/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.shopwave.accountservice.repository.user;
+
+import com.shopwave.accountservice.model.entities.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByEmail(String email);
+}
